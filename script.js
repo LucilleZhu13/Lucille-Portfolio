97dd91c5a6796def84f7540f8660f01be13f8f10
Jiangyue Zhu <jzhu484@gatech.edu>
2025-01-13 10:52:06 -0500

    worked on gallery page

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function jumptoMore(){
     const element = document.getElementById('more');
     element.scrollIntoView({ behavior: 'smooth', block: 'start' }); 
 }
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
