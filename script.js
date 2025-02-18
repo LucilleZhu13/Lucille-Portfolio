fc77aefc19eae6ff4bd9560592ae75b1be019fd1
Jiangyue Zhu <jzhu484@gatech.edu>
2025-02-18 12:02:09 -0500

    update gallery page

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ function jumptoMore(){
 
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
